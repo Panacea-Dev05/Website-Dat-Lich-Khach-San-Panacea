9022b851fe8d60bae8b17e84ff349989a046e534
Tuanna9 <tuanmon6886@gmail.com>
2025-06-11 00:08:13 +0700

    Initial commit

---
+++ src/main/java/panacea/website_dat_lich_khach_san/WebsiteDatLichKhachSanApplication.java
@@ -0,0 +1,13 @@
+package panacea.website_dat_lich_khach_san;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDatLichKhachSanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteDatLichKhachSanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panacea/website_dat_lich_khach_san/WebsiteDatLichKhachSanApplicationTests.java
@@ -0,0 +1,13 @@
+package panacea.website_dat_lich_khach_san;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDatLichKhachSanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
